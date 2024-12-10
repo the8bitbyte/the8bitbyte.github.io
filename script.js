86540cf8e33ca825672d7860190e58207289f845
the8bitbyte <133459899+the8bitbyte@users.noreply.github.com>
2024-12-10 15:20:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const usernameElement = document.getElementById("username");
-const textList = ["The8bitbyte", "Zeus_gameover", "rAEuyq5NrpNFT6BY", "NotSoMeanPlease"]; // List of strings
-let currentIndex = 0;
+//const usernameElement = document.getElementById("username");
+//const textList = ["The8bitbyte", "Zeus_gameover", "rAEuyq5NrpNFT6BY", "NotSoMeanPlease"]; // List of strings
+//let currentIndex = 0;
 
-function changeText() {
+//function changeText() {
   // Fade out
-  usernameElement.style.opacity = 0;
+  //usernameElement.style.opacity = 0;
 
   // Wait for the fade-out transition to finish, then change text and fade in
-  setTimeout(() => {
-    currentIndex = (currentIndex + 1) % textList.length; // Loop through the list
-    usernameElement.textContent = textList[currentIndex]; // Set the new text
-    usernameElement.style.opacity = 1; // Fade in
-  }, 1000); // Timeout matches the fade-out duration
-}
+  //setTimeout(() => {
+    //currentIndex = (currentIndex + 1) % textList.length; // Loop through the list
+    //usernameElement.textContent = textList[currentIndex]; // Set the new text
+    //usernameElement.style.opacity = 1; // Fade in
+  //}, 1000); // Timeout matches the fade-out duration
+//}
 
 // Run the loop every 3 seconds
-setInterval(changeText, 3000);
+//setInterval(changeText, 3000);
