9acb661e5f34cc1998a0dc0d6fc093b15487dde8
the8bitbyte <133459899+the8bitbyte@users.noreply.github.com>
2024-12-24 19:15:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,97 +30,116 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+function getOperatingSystem() {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+        return "Other";
+    }
+
+const os = getOperatingSystem();
+
+if (os !== "iOS" && os !== "Android") {
+    let amnt = 12;
+    var dots = [],
+        mouse = {
+            x: 0,
+            y: 0
+        };
 
 
+    function interpolatePurpleHue(startHue, index, total) {
+        const hueMin = 269.88;
+        const hueMax = 300;
+        const hueRange = hueMax - hueMin;
+        const hueIncrement = hueRange / total;
+        const hue = startHue + index * hueIncrement;
+        return Math.min(Math.max(hue, hueMin), hueMax);
+    }
 
 
+    const startHue = 269.88;
 
-let amnt = 12;
-var dots = [],
-    mouse = {
-        x: 0,
-        y: 0
+
+    var Dot = function(index) {
+        this.x = 0;
+        this.y = 0;
+        this.node = (function() {
+            var n = document.createElement("div");
+            n.className = "trail";
+            n.id = "trail-el";
+
+
+            const hue = interpolatePurpleHue(startHue, index, amnt);
+            n.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+            document.body.appendChild(n);
+            return n;
+        }());
+    };
+
+    Dot.prototype.draw = function() {
+        this.node.style.left = this.x + "px";
+        this.node.style.top = this.y + "px";
     };
 
 
-function interpolatePurpleHue(startHue, index, total) {
-    const hueMin = 269.88;
-    const hueMax = 300;
-    const hueRange = hueMax - hueMin;
-    const hueIncrement = hueRange / total;
-    const hue = startHue + index * hueIncrement;
-    return Math.min(Math.max(hue, hueMin), hueMax);
-}
+    for (var i = 0; i < amnt; i++) {
+        var d = new Dot(i);
+        dots.push(d);
+    }
 
 
-const startHue = 269.88;
+    function draw() {
+        var x = mouse.x,
+            y = mouse.y;
 
 
-var Dot = function(index) {
-    this.x = 0;
-    this.y = 0;
-    this.node = (function() {
-        var n = document.createElement("div");
-        n.className = "trail";
-        n.id = "trail-el";
+        dots.forEach(function(dot, index, dots) {
+            var nextDot = dots[index + 1] || dots[0];
+
+            dot.x = x;
+            dot.y = y;
+            dot.draw();
+            x += (nextDot.x - dot.x) * 0.6;
+            y += (nextDot.y - dot.y) * 0.6;
+        });
+    }
 
 
-        const hue = interpolatePurpleHue(startHue, index, amnt);
-        n.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
-        document.body.appendChild(n);
-        return n;
-    }());
-};
-
-Dot.prototype.draw = function() {
-    this.node.style.left = this.x + "px";
-    this.node.style.top = this.y + "px";
-};
-
-
-for (var i = 0; i < amnt; i++) {
-    var d = new Dot(i);
-    dots.push(d);
-}
-
-
-function draw() {
-    var x = mouse.x,
-        y = mouse.y;
-
-
-    dots.forEach(function(dot, index, dots) {
-        var nextDot = dots[index + 1] || dots[0];
-
-        dot.x = x;
-        dot.y = y;
-        dot.draw();
-        x += (nextDot.x - dot.x) * 0.6;
-        y += (nextDot.y - dot.y) * 0.6;
+    addEventListener("mousemove", function(event) {
+        mouse.x = event.pageX - 3;
+        mouse.y = event.pageY;
     });
+
+
+    let lastScrollY2 = window.scrollY;
+    window.addEventListener('scroll', () => {
+        const scrollDiff = window.scrollY - lastScrollY2;
+        lastScrollY2 = window.scrollY;
+
+        mouse.y += scrollDiff;
+        draw();
+    });
+
+
+    function animate() {
+        draw();
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
 }
 
 
-addEventListener("mousemove", function(event) {
-    mouse.x = event.pageX - 3;
-    mouse.y = event.pageY;
-});
 
 
-let lastScrollY2 = window.scrollY;
-window.addEventListener('scroll', () => {
-    const scrollDiff = window.scrollY - lastScrollY2;
-    lastScrollY2 = window.scrollY;
-
-    mouse.y += scrollDiff;
-    draw();
-});
 
 
-function animate() {
-    draw();
-    requestAnimationFrame(animate);
-}
 
-animate();
 
